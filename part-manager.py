79a23e03e6e213465f3e1f1a270aab61587f7ef3
AliE99 <alielahi78@yahoo.com>
2020-07-19 19:26:32 +0430

    Label and entry inserted

---
+++ part-manager.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+# Create Window object
+app = Tk()
+
+app.title('Part Manager')
+app.geometry('600x300')
+
+# Part
+part_text = StringVar()
+part_label = Label(app, text='Part Name', pady=20, padx=10)
+part_label.grid(row=0, column=0, sticky=W)
+part_entry = Entry(app, textvariable=part_text)
+part_entry.grid(row=0, column=1)
+
+# Customer
+customer_text = StringVar()
+customer_label = Label(app, text='Customer', pady=20, padx=10)
+customer_label.grid(row=0, column=2, sticky=W)
+customer_entry = Entry(app, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
+
+# Retailer
+retailer_text = StringVar()
+retailer_label = Label(app, text='Retailer', padx=10)
+retailer_label.grid(row=1, column=0, sticky=W)
+retailer_entry = Entry(app, textvariable=retailer_text)
+retailer_entry.grid(row=1, column=1)
+
+# Price
+price_text = StringVar()
+price_label = Label(app, text='Price', padx=10)
+price_label.grid(row=1, column=2, sticky=W)
+price_entry = Entry(app, textvariable=price_text)
+price_entry.grid(row=1, column=3)
+
+# Start program
+app.mainloop()
